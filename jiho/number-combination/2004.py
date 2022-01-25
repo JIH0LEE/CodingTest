905891957abfbe2b192a181507246280ad9988f6
JIH0LEE <skql5810@gmail.com>
2022-01-25 17:33:58 +0900

    끝

--- jiho/number-combination/2004.py
+++ jiho/number-combination/2004.py
@@ -1,22 +1,29 @@
-#조합 0의 개수
-#메모리 초과?
-#시간 초과?
+# 조합 0의 개수
+# 메모리 초과?
+# 시간 초과?
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-n,m=map(int,input().split())
-def find_count(n,m,k):
-    count=0
-    for i in range(m+1,n+1):
-        num=i
-        while True:
-            if num%k !=0:
-                break
-            count+=1
-            num=num//k
+n, m = map(int, input().split())
 
-    return count
-five_count=find_count(n,n-m,5)-find_count(m,1,5)
-two_count=find_count(n,n-m,2)-find_count(m,1,2)
 
-print(min(five_count,two_count))
+def find_count(n):
+    count2 = 0
+    count5 = 0
+    num2 = 2
+    num5 = 5
+    while num2 <= n:
+        count2 += (n//num2)
+        num2 = 2*num2
+    while num5 <= n:
+        count5 += (n//num5)
+        num5 = 5*num5
+
+    return (count2, count5)
+
+
+a = find_count(n)
+b = find_count(n-m)
+c = find_count(m)
+
+print(min(a[0]-b[0]-c[0], a[1]-b[1]-c[1]))
--- jiho/number-combination/2981.py
+++ jiho/number-combination/2981.py
@@ -1,22 +1,23 @@
 # 검문
-# 시간 초과 
 import sys
+from math import gcd
 input = sys.stdin.readline
+
 n = int(input())
 inputs = []
-result = []
 for i in range(n):
     inputs.append(int(input()))
+result = inputs[1]-inputs[0]
+outputs = set()
+for i in range(2, n):
+    result = gcd(result, abs(inputs[i]-inputs[i-1]))
+for i in range(1, int(round(result**0.5))+1):
+    if result % i == 0:
 
-inputs.sort()
-min1=inputs[0]
-min2=inputs[1]
-for i in range(2, min2+1):
-    temp = min1 % i
-    flag = True
-    for elem in inputs[1:]:
-        if elem % i != temp:
-            flag = False
-            break
-    if flag:
-        print(i, end=" ")
+        outputs.add(i)
+        outputs.add(result//i)
+
+new = list(outputs)
+new.sort()
+for elem in new[1:]:
+    print(elem, end=" ")

d64f2d34a08e5fe0abc9557d5775332927b16a6f
박소현 <shpark5938@naver.com>
2022-02-08 00:33:39 +0900

    stack & queue

---
+++ sohyun/Week5/stack/4949_균형잡힌세상.py
@@ -0,0 +1,27 @@
+while True:
+    inp = input()
+    stack = []
+
+    if inp == ".":
+        break
+
+    for i in inp:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                break
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')

ce747db433c59b8eb98db6da711bc7f6259366e1
박소현 <shpark5938@naver.com>
2022-02-08 17:33:52 +0900

    2/8 stack (x)

---
+++ sohyun/Week5/stack/1874_스택수열(x).py
@@ -0,0 +1,25 @@
+n = int(input())
+count = 0
+stack = []
+res = []
+impossible = True
+
+for i in range(n):
+    s = int(input())
+
+    while count <= s:
+      count += 1
+      stack.append(count)
+      res.append('+')
+
+    if s == stack[-1]:
+        stack.pop()
+        res.append('-')
+    else:
+        impossible = False
+        exit(0)
+
+if impossible == False:
+    print('NO')
+else:
+    print('\n'.join(res))

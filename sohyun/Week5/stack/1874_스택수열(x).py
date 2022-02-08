736240b3e69959e525e7778e164fd8730788bed0
박소현 <shpark5938@naver.com>
2022-02-09 01:05:10 +0900

    fix stack

--- sohyun/Week5/stack/1874_스택수열(x).py
+++ sohyun/Week5/stack/1874_스택수열(x).py
@@ -2,24 +2,24 @@ n = int(input())
 count = 0
 stack = []
 res = []
-impossible = True
+# impossible = True
 
-for i in range(n):
+for i in range(0, n):
     s = int(input())
 
-    while count <= s:
-      count += 1
-      stack.append(count)
-      res.append('+')
+    while count < s:
+        count += 1
+        stack.append(count)
+        res.append("+")
 
     if s == stack[-1]:
         stack.pop()
-        res.append('-')
-    else:
-        impossible = False
-        exit(0)
+        res.append("-")
+    # else:
+    #     impossible = False
+    #     exit(0)
 
-if impossible == False:
-    print('NO')
-else:
-    print('\n'.join(res))
+# if impossible == False:
+#     print("NO")
+# else:
+print("\n".join(res))

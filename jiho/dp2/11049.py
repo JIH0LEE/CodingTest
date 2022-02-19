d23c11ed07062c39aa362c7cbf359f76f2d67c73
JIH0LEE <skql5810@gmail.com>
2022-02-19 13:35:23 +0900

    dp2 -1

---
+++ jiho/dp2/11049.py
@@ -0,0 +1,21 @@
+#행렬 곱셈 순서
+import sys
+input=sys.stdin.readline
+n=int(input())
+mat=[]
+for i in range(n):
+    mat.append(list(map(int,input().split())))
+#dp[i][j] : j 부터 i+j 까지의 곱의 최소 연산수
+dp=[[0 for _ in range(n)]]
+for i in range(1,n):
+    new_dp=[]
+    for j in range(n-i):
+        temp=[dp[k][j]+dp[i-k-1][j+k+1]+mat[j][0]*mat[j+k][1]*mat[i+j][1] for k in range(0,i)]
+        min_value=min(temp)
+        new_dp.append(min_value)
+    dp.append(new_dp)
+
+print(dp[n-1][0])
+
+
+
---
+++ jiho/dp2/11066.py
@@ -0,0 +1,25 @@
+#파일합치기
+#pypy3통과
+#python3 시간초과 ㅠㅠ
+import sys
+input=sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    dp = [[]]
+    input_files = list(map(int, input().split()))
+    sum = [0]
+    for elem in input_files:
+        sum.append(sum[-1]+elem)
+    temp = [0]*(n+1)
+    dp.append(temp)
+    for i in range(2, n+1):
+        new_dp = [0]
+        for j in range(1, n-i+2):
+            mid_sum=sum[i+j-1]-sum[j-1]
+            temp=[dp[k][j]+dp[i-k][k+j]+mid_sum for k in range(1,i)]
+            min_num=min(temp)
+            new_dp.append(min_num)
+        dp.append(new_dp)
+    print(dp[n][1])
